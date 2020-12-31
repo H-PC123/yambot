884f5d1e095630b1145a19d8a03b2b963f58b8f1
Paul C <pc123321123@cumallover.me>
2020-12-31 09:36:40 -0500

    Added voice channel joining

--- main.py
+++ main.py
@@ -14,8 +14,25 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$What's ligma?"):
+    if message.content.startswith("What's ligma?"):
         await message.channel.send('Ligma balls')
+
+    if message.content.startswith("join vc"):
+        try:
+            author_vc = message.author.voice.channel
+            await message.channel.send("Coming on!")
+            await author_vc.connect()
+        except AttributeError:
+            await message.channel.send("Join a channel first!")
+    else:
+        return
+
+    if message.content.startswith("get out bot"):
+        await message.channel.send("ok")
+        author_vc = message.author.voice.channel
+        await author_vc.disconnect()
+
+
 
 
 client.run("NzkzMTQ2MzM2MDU5OTgxODI1.X-oBFg.0x3_8-GaFdpdiINSIpJSX6WYgrE")
