07d52cbc625c48b17e7943911b2a67a9594199bf
hpc123 <chen.paulb@gmail.com>
2023-01-29 23:42:31 -0500

    Changed main to be accept script parameter and be a bit prettier in general

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+# TODO: restructure to standard
+import sys
 import json
 from bot import yamBot
 
-myBot = yamBot(command_prefix='.')
 
-with open("secrets.json", "r") as f:
-    secrets = json.load(f)
-    print(secrets)
-    bot_token = secrets["bot_token"]
+def start_yam(args):
+    # should yamBot handle with default intents or should we pass something here to the init call?
+    myYam = yamBot(command_prefix='.')
 
-#TODO: handle token passing from program call
+    if args:
+        bot_token = args[0]
+    else:
+        with open("secrets.json", "r") as f:
+            secrets = json.load(f)
+            print(secrets)
+            bot_token = secrets["bot_token"]
 
-myBot.run(bot_token)
+    myYam.run(bot_token)
+
+
+if __name__ == "__main__":
+    start_yam(sys.argv[1:])
