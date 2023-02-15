2939f4ed716f0900b3a333f6b47b9ef01e5634fe
hpc123 <chen.paulb@gmail.com>
2023-02-15 00:31:47 -0500

    Fixed locking on downloads ~~Added song name getting from metadata~~ nvm, underestimated id3 tag's mysteriousness Changed download strategy to use random alphanum. strings

--- yam_player.py
+++ yam_player.py
@@ -1,8 +1,11 @@
 import os
 import logging
+import string
 
 import discord
 import urllib3
+import mutagen
+import random
 import threading
 import concurrent.futures
 from discord.ext import commands
@@ -13,16 +16,17 @@ class YamPlayer(commands.Cog):
     # TODO: Need to add Rlock to playlist and make sure methods that add to the playlist use the lock
     playable_extensions = ["mp3"]
     media_folder = os.path.join(os.getcwd(), "Downloads")
-    pool_manager = urllib3.PoolManager()
+    pool_manager = urllib3.PoolManager(maxsize=3)
 
     dest_lock = threading.RLock()
-    playlist.lock = []
+
     # Will lock a song from being overwritten in the playlist by only releasing after song is played
 
-    def __init__(self, media_folder=os.path.join(os.getcwd(), "Downloads"), chunk_size=2048):
+    def __init__(self, media_folder=os.path.join(os.getcwd(), "Downloads"), chunk_size=2048, filename_size=32):
         super().__init__()
         self.media_folder = media_folder
         self.chunk_size = chunk_size
+        self.filename_size = filename_size
         logging.info(f"Media will be downloaded to: {media_folder}")
 
     @commands.command()
@@ -32,11 +36,19 @@ class YamPlayer(commands.Cog):
     @commands.command()
     async def play(self, ctx: commands.Context):
         if ctx.message.attachments:
-            playable_attachments = [x for x in ctx.message.attachments if self.check_file(x.url)]
+            playable_attachments = [x for x in ctx.message.attachments if await self.check_file_extension(x.url)]
             logging.info(f"Message has the following playable files attached: {playable_attachments}")
-            with self.dest_lock and concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-                executor.map(self.store_attachment, playable_attachments)
-                # TODO: Add item to playlist, then hit play
+            with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+                saved_files = [executor.submit(self.store_attachment, x).result() for x in playable_attachments]
+                #executor.map(self.store_attachment, playable_attachments)
+            logging.debug(f"Files saved: {saved_files}")
+
+            song_titles = [x.filename for x in playable_attachments]
+            for song, title in zip(saved_files, song_titles):
+                self.playlist.append((song, title))
+            logging.debug(f"playlist: {self.playlist}")
+
+                # TODO: Actually play the songs
         elif self.check_link(ctx.message):
             # TODO: parse link or file from message, download, add to playlist, play
             logging.info(f"Message contains link potentially containing playable content: {ctx.message.content}")
@@ -44,8 +56,11 @@ class YamPlayer(commands.Cog):
         # self.playlist.append(ctx)
         return NotImplementedError
 
-    def check_file(self, link: str) -> bool:
-        # TODO: should check somehow for security
+    async def check_file(self, file: "path to file") -> bool:
+        # TODO uses mutagen to check if the file is actuallty playable using the metadata
+        return True
+
+    async def check_file_extension(self, link: str) -> bool:
         if link.split('.')[-1] in self.playable_extensions:
             return True
         logging.info(f"File hosted at {link} was not playable")
@@ -55,22 +70,32 @@ class YamPlayer(commands.Cog):
         # TODO: add check for link(s) and if is playable content
         raise NotImplementedError
 
-    def store_attachment(self, att: "Message attachment") -> bool:
+    def store_attachment(self, att: "discord attachment") -> str:
         # Downloads music into self.media_folder
-        # TODO: if file is already in folder, return true and set the appropriate references
-        # TODO: if file is being played or on the queue, they need to be locked so they aren't overwritten
-        logging.debug(f"Attempting download of {att}")
         try:
-            with open(os.path.join(self.media_folder, att.filename), "wb+") as dest, \
+            rand_filename = self.get_rand_filename() + os.path.splitext(att.filename)[-1]
+            logging.debug(f"{rand_filename} : {att.filename}")
+            with self.dest_lock, \
+                    open(os.path.join(self.media_folder, rand_filename), "wb+") as dest, \
                     self.pool_manager.request("GET", att.url, preload_content=False) as req:
+                logging.debug(f"Attempting download of {att}")
                 while True:
                     data = req.read(self.chunk_size)
                     if not data:
                         break
                     dest.write(data)
-            logging.info(f"File: {att.filename} downloaded from: {att.url}")
-            return True
+                logging.info(f"Attachment {att.filename} downloaded from: {att.url}")
+            return rand_filename
         except urllib3.exceptions.HTTPError as e:
             # for handling specific errors (that I cant think of yet)
             logging.debug(f"File: {att.filename} could not be downloaded: {att.url} due to {e}")
-            return False
+            return None
+        logging.debug("failed")
+
+    def get_rand_filename(self) -> str:
+        rand_name = "".join(random.choice(string.ascii_letters + string.digits) for i in range(self.filename_size))
+        return rand_name
+
+    def get_songname(self, file: "song file name") -> str:
+        song_path = os.path.join(self.media_folder, file)
+        return mutagen.File(song_path)["title"]
